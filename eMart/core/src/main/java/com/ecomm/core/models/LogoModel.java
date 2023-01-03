93da08b6e231493bbbf3aab9281646f9195d9cb4
souru012 <sourav@nexteonsolutions.com>
2023-01-03 17:38:10 +0530

    added logo component on header fragment

---
+++ eMart/core/src/main/java/com/ecomm/core/models/LogoModel.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2015 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.ecomm.core.models;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.settings.SlingSettingsService;
+
+import javax.annotation.PostConstruct;
+import java.util.Optional;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+@Model(adaptables = Resource.class)
+public class LogoModel {
+
+    @ValueMapValue(name="fileReference", injectionStrategy=InjectionStrategy.OPTIONAL)
+    protected String src;
+
+    @ValueMapValue(name="linkURL", injectionStrategy=InjectionStrategy.OPTIONAL)
+    protected String href;
+
+
+
+    @PostConstruct
+    protected void init() {
+
+    }
+
+    public String getSrc() { return src; }
+
+    public String getHref() { return href; }
+}
